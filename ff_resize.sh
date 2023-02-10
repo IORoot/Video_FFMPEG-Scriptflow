c342bac5558bc786c3fae6afd6fad8bcf45c3892
Andy Pearson <andy@londonparkour.com>
2023-02-10 14:24:32 +0000

    Scale

--- ff_resize.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-# https://ottverse.com/change-resolution-resize-scale-video-using-ffmpeg/
---
+++ ff_scale.sh
@@ -0,0 +1,157 @@
+#!/bin/bash
+# ╭──────────────────────────────────────────────────────────────────────────────╮
+# │                                                                              │
+# │             Change the scale (physical dimensions) of the video              │
+# │                                                                              │
+# ╰──────────────────────────────────────────────────────────────────────────────╯
+
+# https://ottverse.com/change-resolution-resize-scale-video-using-ffmpeg/
+
+
+# ╭──────────────────────────────────────────────────────────╮
+# │                       Set Defaults                       │
+# ╰──────────────────────────────────────────────────────────╯
+
+set -o errexit                                              # If a command fails bash exits.
+set -o pipefail                                             # pipeline fails on one command.
+if [[ "${DEBUG-0}" == "1" ]]; then set -o xtrace; fi        # DEBUG=1 will show debugging.
+cd "$(dirname "$0")"                                        # Change to the script folder.
+
+# ╭──────────────────────────────────────────────────────────╮
+# │                        VARIABLES                         │
+# ╰──────────────────────────────────────────────────────────╯
+
+OUTPUT_FILENAME="output_scale.mp4"
+WIDTH="1920"
+HEIGHT="1080"
+LOGLEVEL="error" 
+
+# ╭──────────────────────────────────────────────────────────╮
+# │                          Usage.                          │
+# ╰──────────────────────────────────────────────────────────╯
+
+usage()
+{
+    if [ "$#" -lt 2 ]; then
+        printf "ℹ️ Usage:\n $0 -i <INPUT_FILE> [-w <WIDTH>] [-h <HEIGHT>] [-o <OUTPUT_FILE>] [-l loglevel]\n\n" >&2 
+
+        printf "Summary:\n"
+        printf "Change the scale (Width/hEIGHT) of a video.\n\n"
+
+        printf "Flags:\n"
+
+        printf " -i | --input <INPUT_FILE>\n"
+        printf "\tThe name of an input file.\n\n"
+
+
+        printf " -o | --output <OUTPUT_FILE>\n"
+        printf "\tDefault is %s\n" "${OUTPUT_FILENAME}"
+        printf "\tThe name of the output file.\n\n"
+
+
+        printf " -w | --width <PIXELS>\n"
+        printf "\tThe width of the video. The default value is 1920.\n\n"
+
+
+        printf " -h | --height <PIXELS>\n"
+        printf "\tThe height of the video. The default value is 1920.\n\n"
+
+
+        printf " -l | --loglevel <LOGLEVEL>\n"
+        printf "\tThe FFMPEG loglevel to use. Default is 'error' only.\n"
+        printf "\tOptions: quiet,panic,fatal,error,warning,info,verbose,debug,trace\n"
+
+        exit 1
+    fi
+}
+
+
+# ╭──────────────────────────────────────────────────────────╮
+# │         Take the arguments from the command line         │
+# ╰──────────────────────────────────────────────────────────╯
+function arguments()
+{
+    POSITIONAL_ARGS=()
+
+    while [[ $# -gt 0 ]]; do
+    case $1 in
+
+
+        -i|--input)
+            INPUT_FILENAME="$2"
+            shift
+            shift
+            ;;
+
+
+        -o|--output)
+            OUTPUT_FILENAME="$2"
+            shift 
+            shift
+            ;;
+
+
+        -w|--width)
+            WIDTH="$2"
+            shift 
+            shift
+            ;;
+
+
+
+        -h|--height)
+            HEIGHT="$2"
+            shift 
+            shift
+            ;;
+
+
+        -l|--loglevel)
+            LOGLEVEL="$2"
+            shift 
+            shift
+            ;;
+
+
+        -*|--*)
+            echo "Unknown option $1"
+            exit 1
+            ;;
+
+
+        *)
+            POSITIONAL_ARGS+=("$1") # save positional arg back onto variable
+            shift                   # remove argument and shift past it.
+            ;;
+    esac
+    done
+
+}
+
+
+# ╭──────────────────────────────────────────────────────────╮
+# │                                                          │
+# │                      Main Function                       │
+# │                                                          │
+# ╰──────────────────────────────────────────────────────────╯
+function main()
+{
+
+    printf "This will alter the dimensions of the video.\n"
+
+    if [[ -z "${INPUT_FILENAME}" ]]; then 
+        printf "❌ No input file specified. Exiting.\n"
+        exit 1
+    fi
+
+    printf "📐 Changing the size of the video.\n"
+
+    ffmpeg  -v ${LOGLEVEL} -i ${INPUT_FILENAME} -vf scale=${WIDTH}:${HEIGHT} ${OUTPUT_FILENAME}
+
+    printf "✅ New video created: %s\n" "$OUTPUT_FILENAME"
+
+}
+
+usage $@
+arguments $@
+main $@
