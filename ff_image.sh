3d7b583d1cd782261246d19e0802c82e7771dc37
Andy Pearson <andy@londonparkour.com>
2023-03-15 11:02:41 +0000

    Add ff_image.sh

---
+++ ff_image.sh
@@ -0,0 +1,181 @@
+#!/bin/bash
+# ╭──────────────────────────────────────────────────────────────────────────────╮
+# │                                                                              │
+# │      Create a video from an image for a specific amount of time              │
+# │                                                                              │
+# ╰──────────────────────────────────────────────────────────────────────────────╯
+
+# ╭──────────────────────────────────────────────────────────╮
+# │                       Set Defaults                       │
+# ╰──────────────────────────────────────────────────────────╯
+
+set -o errexit                                              # If a command fails bash exits.
+set -o pipefail                                             # pipeline fails on one command.
+if [[ "${DEBUG-0}" == "1" ]]; then set -o xtrace; fi        # DEBUG=1 will show debugging.
+cd "$(dirname "$0")"                                        # Change to the script folder.
+
+
+# ╭──────────────────────────────────────────────────────────╮
+# │                        VARIABLES                         │
+# ╰──────────────────────────────────────────────────────────╯
+
+OUTPUT_FILENAME="output_image.mp4"
+LOGLEVEL="error"
+DURATION="3"
+
+# ╭──────────────────────────────────────────────────────────╮
+# │                          Usage.                          │
+# ╰──────────────────────────────────────────────────────────╯
+
+usage()
+{
+    if [ "$#" -lt 2 ]; then
+        printf "ℹ️ Usage:\n $0 -i <INPUT_IMAGE_FILE>  [-S <START>] [-E <END>] [-o <OUTPUT_FILE>] [-l loglevel]\n\n" >&2 
+
+        printf "Summary:\n"
+        printf "Create a video from an image.\n\n"
+
+        printf "Flags:\n"
+
+        printf " -i | --input <INPUT_FILE>\n"
+        printf "\tThe name of an input file.\n\n"
+
+        printf " -o | --output <OUTPUT_FILE>\n"
+        printf "\tDefault is %s\n" "${OUTPUT_FILENAME}"
+        printf "\tThe name of the output file.\n\n"
+
+        printf " -d | --duration <SECONDS>\n"
+        printf "\Time in seconds of how long the video should be.\n"
+
+        printf " -l | --loglevel <LOGLEVEL>\n"
+        printf "\tThe FFMPEG loglevel to use. Default is 'error' only.\n"
+        printf "\tOptions: quiet,panic,fatal,error,warning,info,verbose,debug,trace\n\n"
+
+        exit 0
+    fi
+}
+
+
+# ╭──────────────────────────────────────────────────────────╮
+# │         Take the arguments from the command line         │
+# ╰──────────────────────────────────────────────────────────╯
+function arguments()
+{
+    POSITIONAL_ARGS=()
+
+    while [[ $# -gt 0 ]]; do
+    case $1 in
+
+
+        -i|--input)
+            INPUT_FILENAME="$2"
+            shift
+            shift
+            ;;
+
+
+        -o|--output)
+            OUTPUT_FILENAME="$2"
+            shift 
+            shift
+            ;;
+
+
+        -d|--duration)
+            DURATION="$2"
+            shift 
+            shift
+            ;;
+
+
+        -C|--config)
+            CONFIG_FILE="$2"
+            shift 
+            shift
+            ;;
+
+
+        -l|--loglevel)
+            LOGLEVEL="$2"
+            shift 
+            shift
+            ;;
+
+
+        -*|--*)
+            echo "Unknown option $1"
+            exit 1
+            ;;
+
+
+        *)
+            POSITIONAL_ARGS+=("$1") # save positional arg back onto variable
+            shift                   # remove argument and shift past it.
+            ;;
+    esac
+    done
+
+}
+
+
+
+# ╭──────────────────────────────────────────────────────────╮
+# │        Read config-file if supplied. Requires JQ         │
+# ╰──────────────────────────────────────────────────────────╯
+function read_config()
+{
+    # Check if config has been set.
+    if [ -z ${CONFIG_FILE+x} ]; then return 0; fi
+    
+    # Check dependencies
+    if ! command -v jq &> /dev/null; then
+        printf "JQ is a dependency and could not be found. Please install JQ for JSON parsing. Exiting.\n"
+        exit
+    fi
+
+    # Read file
+    LIST_OF_INPUTS=$(cat ${CONFIG_FILE} | jq -r 'to_entries[] | ["--" + .key, .value] | @sh' | xargs) 
+
+    # Print to screen
+    printf "🎛️  Config Flags: %s\n" "$LIST_OF_INPUTS"
+
+    # Sen to the arguments function again to override.
+    arguments $LIST_OF_INPUTS
+}
+
+
+# ╭──────────────────────────────────────────────────────────╮
+# │   Exit the app by just skipping the ffmpeg processing.   │
+# │            Then copy the input to the output.            │
+# ╰──────────────────────────────────────────────────────────╯
+function exit_gracefully()
+{
+    cp -f ${INPUT_FILENAME} ${OUTPUT_FILENAME}
+    exit 0
+}
+
+# ╭──────────────────────────────────────────────────────────╮
+# │                                                          │
+# │                      Main Function                       │
+# │                                                          │
+# ╰──────────────────────────────────────────────────────────╯
+function main()
+{
+
+    if [[ -z "${INPUT_FILENAME}" ]]; then 
+        printf "❌ No input file specified. Exiting.\n"
+        exit_gracefully
+    fi
+
+    printf "🎨 ff_image.sh - Creating a video from an image."
+
+    ffmpeg -v ${LOGLEVEL} -loop 1 -i ${INPUT_FILENAME} -c:v libx264 -t ${DURATION} -pix_fmt yuv420p ${OUTPUT_FILENAME}
+    
+    printf "✅ %s\n" "${OUTPUT_FILENAME}"
+
+}
+
+usage $@
+arguments $@
+read_config "$@"
+main $@
