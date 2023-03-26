5078bc0dd6d1fc0dbde030dc3f708bd0e994dbc5
Andy Pearson <andy@londonparkour.com>
2023-03-26 14:35:22 +0100

    setup ffmpeg

--- templates/ft_youth.sh
+++ templates/ft_youth.sh
@@ -163,10 +163,12 @@ function arguments()
 # ╰──────────────────────────────────────────────────────────╯
 function is_movie_file()
 {
+    FILE=$1
+    
     echo "test"
     ffprobe -version
+    ffprobe -v quiet -select_streams v:0 -show_entries stream=codec_name -print_format csv=p=0 "${FILE}"
 
-    FILE=$1
     if ffprobe -v quiet -select_streams v:0 -show_entries stream=codec_name -print_format csv=p=0 "${FILE}"; then
         return 0
     else
