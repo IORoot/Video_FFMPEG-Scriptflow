456c5f5b676e279edd0bc4f7b7e50d8f3831f4ee
Andy Pearson <andy@londonparkour.com>
2023-04-09 09:19:43 +0100

    update runner

--- ff_download.sh
+++ ff_download.sh
@@ -177,6 +177,10 @@ function exit_gracefully()
 function configure_strategy()
 {
     INPUT_FILE=$1
+    
+    echo "STRAT:${STRATEGY}"
+    echo "TMP_FILE:${TMP_FILE}"
+    ls -la "${TMP_FILE}"
 
     # Randomise and select single download
     if [[ "${STRATEGY}" == "random" ]]; then 
